0bdac1bca10a2b8ae3d4c3df5c2f086b86910e8b
Sumitmasih130 <126502152+Sumitmasih130@users.noreply.github.com>
2023-03-08 17:31:13 +0545

    Add files via upload

---
+++ assigment 1.py
@@ -0,0 +1,61 @@
+#ASSIGNMENT 1
+ 
+Q1 Who developed Python Programming Language?# ans. Guido van Rossum developed python programming language.
+
+Q2. Which type of programming does python support?
+ans. object-oriented, structured, and functional programming.
+ 
+Q3.Is python case sensitive while dealing with identifiers?
+ans. yes, python is a case sensitive while dealing with identifiers.
+ 
+Q4. What is the correct extension of the python file?
+ans. The correct extension of the python file is .py 
+ 
+Q5. Is python code compiled or interpreted?
+ans. Python code is interpreted.
+ 
+Q6. Name a few blocks of code used to define in python language.
+ans. a module, a function body, and a class definition.
+ 
+Q7. State a character used to give single line comment in python. 
+ans. # or ##
+
+Q8.Mention functions which can help us to find the version of python
+that we are currently working on.
+ans. 1. Using sys.version method
+     2. Using python_version() function
+     3. Using python -V command
+      
+Q9. python supports the creation of anonymous functions at run time,using a construct called_______.
+ans. LAMBDA
+  
+Q10. what does PIP stand for in python?
+ans. pip Install Packages.
+
+Q11. Mention few built in functions in python.
+ans. Print()
+      type()
+      len()
+      range()
+      sum()
+      max()
+      sorted()
+   
+Q12. what is the maximum possible lengh of an identifier in python?
+ans. The maximum possible length of an identifier in Python is not specified.
+
+Q13. what are the benefits of using python?
+ans. Easy to Learn and Use, Large and Active Community, Versatility, Great for Data Analysis and Machine Learning, Open-Source etc.
+
+Q14   how is memory managed in python?
+ans.  memory management in Python is automatic and transparent to the programmer. However, it is essential to be aware of memory usage in         programs that create a large number of objects or use large amounts of data to prevent memory leaks and optimize performance.
+
+Q15. how to install python on windows and set path variables?
+ans. To install Python on Windows and set path variables, you need to download the Python installer from the official Python website, run        the installer, and follow the installation wizard. During the installation process, you can choose to add Python to the system path,        which is recommended. If Python is not added to the path, you can set the path variables manually by adding the path to the Python          executable to the "Path" system variable. Once you have set the path variables, you can open a new Command Prompt and type "python" to      verify that Python is installed and the path variables are set correctly.
+
+Q16. Is indentation required in python?
+ans. yes,indentation is required in python.
+
+
+
+
